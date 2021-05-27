56d9302cfefa439d90412b58896a3526d5166f82
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-27 12:50:33 +0100

    update evergreen_bot with quit command

--- lib/evergreen_bot.rb
+++ lib/evergreen_bot.rb
@@ -6,6 +6,8 @@ Telegram::Bot::Client.run(TOKEN_API) do |bot|
     case message.text
     when '/start'
       bot.api.send_message(chat_id: message.chat.id, text: "Welcome to Evergreenbot, I ma a multivational bot. my commands are /lifehack /morning_mutivation /Programming_quotes /quit /Confirm /Exit")
+    when '/quit'
+      bot.api.send_message(chat_id: message.chat.id, text: "Are you sure you want to quit this early? enter /Confirm to quit")
     end
   end
 end
