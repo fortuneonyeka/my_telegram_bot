f0b0b0c8c4ffd2a7033f597c9c154cfd1b6c2d23
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-28 00:46:58 +0100

    fix linters

--- lib/evergreen_bot.rb
+++ lib/evergreen_bot.rb
@@ -12,7 +12,8 @@ class Evergreenbot
       bot.listen do |message|
         case message.text
         when '/start'
-          bot.api.send_message(chat_id: message.chat.id, text: 'Welcome to Evergreenbot, I ma a motivational bot. my commands are, /lifehack, /morning_motivation ,/Programming_quotes, /quit, /Confirm /Exit')
+          bot.api.send_message(chat_id: message.chat.id,
+                               text: 'Welcome to Evergreenbot, I ma a motivational bot. my commands are, /lifehack, /morning_motivation ,/Programming_quotes, /quit, /Confirm /Exit')
         when '/quit'
           bot.api.send_message(chat_id: message.chat.id, text: 'Are you sure you want to quit this early? enter /Confirm to quit')
         when '/Confirm'
