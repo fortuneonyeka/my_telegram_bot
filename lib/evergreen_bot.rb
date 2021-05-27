ecb289bc7b54a702f7dd7b9b4de9cd731aae70f9
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-27 12:49:23 +0100

    update evergreen_bot with start command with all the available command options

--- lib/evergreen_bot.rb
+++ lib/evergreen_bot.rb
@@ -1,4 +1,11 @@
 require 'telegram/bot'
 require_relative '../config.rb'
 
-
+Telegram::Bot::Client.run(TOKEN_API) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Welcome to Evergreenbot, I ma a multivational bot. my commands are /lifehack /morning_mutivation /Programming_quotes /quit /Confirm /Exit")
+    end
+  end
+end
