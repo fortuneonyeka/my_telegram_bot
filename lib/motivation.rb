0c634d896ab049eee17077f30feb456eb1e303e4
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-29 00:11:28 +0100

    update movtivation with space after commas

--- lib/motivation.rb
+++ lib/motivation.rb
@@ -7,7 +7,7 @@ class Motivation
 
   def initialize
     @morning_motivation = [
-      'Love for all, hate for none','Every momment is a fresh beginning ', 'Whatever you do, do it well', 'What we think,we become'
+      'Love for all, hate for none', 'Every momment is a fresh beginning ', 'Whatever you do, do it well', 'What we think,we become'
     ]
   end
 
